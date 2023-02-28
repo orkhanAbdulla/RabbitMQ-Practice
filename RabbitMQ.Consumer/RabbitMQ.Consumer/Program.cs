e2479e17b20dd58757f2b91dd17a46722dd2f151
orkhanAbdulla <Orkhanaa@code.edu.az>
2023-03-01 02:53:00 +0400

    Add Project files

---
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿//Bağlantı oluşturma
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory Factory = new();
+Factory.Uri = new("amqps://qryvqupu:qNwJLRjbEFh9MBNM2BGIyikyyPEcxVXz@chimpanzee.rmq.cloudamqp.com/qryvqupu");
+
+//Bağlantını aktiv etmək və Kanal açnaq
+
+using IConnection connection = Factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue Oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);//Consumer-da da queue publisherde olduqu kimi bire bire eyni qeyd olunmalidi
+
+// Queue-dən messaj Oxuma
+
+EventingBasicConsumer consumer=new(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer);
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+Console.Read();
+
+
---
+++ RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+
+//Bağlantı oluşturma
+ConnectionFactory Factory = new();
+Factory.Uri = new("amqps://qryvqupu:qNwJLRjbEFh9MBNM2BGIyikyyPEcxVXz@chimpanzee.rmq.cloudamqp.com/qryvqupu");
+
+//Bağlantını aktiv etmək və Kanal açnaq
+
+using IConnection connection = Factory.CreateConnection();
+using IModel channel=connection.CreateModel();
+
+//Queue Oluşturma
+
+channel.QueueDeclare(queue: "example-queue",exclusive:false);
+
+// Queue-ya Mesaj Gönderme
+
+//RabbitMQ queue-ya atacaqi mesajları byte tipinən qəbul etməkdədir.
+
+byte[] message=Encoding.UTF8.GetBytes("Merhaba");
+channel.BasicPublish(exchange:"",routingKey:"example-queue",body:message);
+
+Console.Read();
+
+
